1e5ec876196bd96fa57313efa9d9e12c7e8586cb
Sverre Johann Bjørke <sverre.bjorke@knowit.no>
2023-08-10 16:33:33 +0200

    skia? bytes? reqwest?

--- src/main.rs
+++ src/main.rs
@@ -30,44 +30,12 @@ fn main() -> Result<(), slint::PlatformError> {
             let ui = ui_handle2.unwrap();
             ui.set_xkcdTitle("jeje".into());
 
-            let mut pixel_buffer = SharedPixelBuffer::<Rgba8Pixel>::new(640, 480);
-            let width = pixel_buffer.width();
-            let height = pixel_buffer.height();
-            let mut pixmap =
-                tiny_skia::PixmapMut::from_bytes(pixel_buffer.make_mut_bytes(), width, height)
-                    .unwrap();
-            pixmap.fill(tiny_skia::Color::TRANSPARENT);
+            let img_bytes = reqwest::blocking::get("https://imgs.xkcd.com/comics/what_to_do.png")
+                .unwrap()
+                .bytes()
+                .unwrap();
 
-            let circle = tiny_skia::PathBuilder::from_circle(320., 240., 150.).unwrap();
-
-            let mut paint = tiny_skia::Paint::default();
-            paint.shader = tiny_skia::LinearGradient::new(
-                tiny_skia::Point::from_xy(100.0, 100.0),
-                tiny_skia::Point::from_xy(400.0, 400.0),
-                vec![
-                    tiny_skia::GradientStop::new(
-                        0.0,
-                        tiny_skia::Color::from_rgba8(50, 127, 150, 200),
-                    ),
-                    tiny_skia::GradientStop::new(
-                        1.0,
-                        tiny_skia::Color::from_rgba8(220, 140, 75, 180),
-                    ),
-                ],
-                tiny_skia::SpreadMode::Pad,
-                tiny_skia::Transform::identity(),
-            )
-            .unwrap();
-
-            pixmap.fill_path(
-                &circle,
-                &paint,
-                tiny_skia::FillRule::Winding,
-                Default::default(),
-                None,
-            );
-
-            let image = Image::from_rgba8_premultiplied(pixel_buffer);
+            let mut pixmap = tiny_skia::PixmapMut::from_bytes(img_bytes, width, height).unwrap();
 
             ui.set_xkcdImage(image);
         },
