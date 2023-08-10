db0648ab96180189d8b21b7cf226bafc4f7e54fe
Sverre Johann Bjørke <sverre.bjorke@knowit.no>
2023-08-10 18:03:13 +0200

    anakin.gif

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 slint::include_modules!();
+use image::{io::Reader as ImageReader, ColorType, GenericImageView};
+
 use slint::{Image, Rgba8Pixel, SharedPixelBuffer, Timer, TimerMode};
+use std::io::Cursor;
+use tiny_skia::PixmapMut;
+
 extern crate chrono;
 
 use chrono::Local;
@@ -30,13 +35,28 @@ fn main() -> Result<(), slint::PlatformError> {
             let ui = ui_handle2.unwrap();
             ui.set_xkcdTitle("jeje".into());
 
-            let img_bytes = reqwest::blocking::get("https://imgs.xkcd.com/comics/what_to_do.png")
+            let response = reqwest::blocking::get("https://imgs.xkcd.com/comics/what_to_do_2x.png")
+                .expect("Failed to download image");
+
+            let image_data = response.bytes().expect("Failed to read image data");
+
+            // Wrap the image data in a `Cursor` to allow reading from it
+            let cursor = Cursor::new(image_data.as_ref());
+
+            // Decode the image into a `RgbaImage` from the `image` crate
+            let dynamic_image = ImageReader::new(cursor)
+                .with_guessed_format()
                 .unwrap()
-                .bytes()
+                .decode()
                 .unwrap();
+            let rgba_image = dynamic_image.into_rgba8();
 
-            let mut pixmap = tiny_skia::PixmapMut::from_bytes(img_bytes, width, height).unwrap();
-
+            let buffer = SharedPixelBuffer::<Rgba8Pixel>::clone_from_slice(
+                rgba_image.as_raw(),
+                rgba_image.width(),
+                rgba_image.height(),
+            );
+            let image = Image::from_rgba8(buffer);
             ui.set_xkcdImage(image);
         },
     );
