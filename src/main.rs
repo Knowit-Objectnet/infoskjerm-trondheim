ecd0ef2ef9da171833433833f91eac89d5bc8294
Sverre Johann Bjørke <sverre.bjorke@knowit.no>
2023-08-13 14:54:09 +0200

    xkcd widget

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ mod xkcd;
 use crate::xkcd::*;
 
 fn main() -> Result<(), slint::PlatformError> {
-    let ui = AppWindow::new()?;
+    let ui = MainWindow::new()?;
     let clock_timer = Timer::default();
     let xkcd_timer = Timer::default();
 
@@ -28,9 +28,9 @@ fn main() -> Result<(), slint::PlatformError> {
     );
 
     let xkcd = get_current_xkcd();
+    ui.set_xkcdTitle(xkcd.title.into());
     ui.set_xkcdFlavorText(xkcd.flavor_text.into());
     ui.set_xkcdImage(xkcd.image);
-    ui.set_xkcdTitle(xkcd.title.into());
 
     xkcd_timer.start(
         TimerMode::Repeated,
