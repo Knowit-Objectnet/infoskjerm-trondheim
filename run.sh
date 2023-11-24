3d3edf51dd5f946e9852cacf0c31ce2a3c63f54c
Sverre Johann Bjørke <sverre.bjorke@knowit.no>
2023-11-24 11:54:44 +0100

    update run script with wait for wifi

--- run.sh
+++ run.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 export DISPLAY=:0
 export SLINT_FULLSCREEN=1
+
+# Function to check Wi-Fi connection
+check_wifi() {
+    if ! iwconfig wlan0 | grep -q "ESSID:off/any"; then
+        return 0  # Wi-Fi is connected
+    else
+        return 1  # Wi-Fi is not connected
+    fi
+}
+
+# Wait for Wi-Fi connection
+while ! check_wifi; do
+    echo "Waiting for Wi-Fi connection..."
+    sleep 5
+done
+
 /home/infoskjerm/bin/infoskjerm &
