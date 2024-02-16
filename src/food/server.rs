1072b9f652dc15faa87b83820173df9a8805dcc5
Sverre Johann Bjørke <sverrejb@gmail.com>
2024-02-16 09:56:37 +0100

    fix wolt API url

--- src/food/server.rs
+++ src/food/server.rs
@@ -4,6 +4,9 @@ use reqwest::Url;
 use serde::Deserialize;
 use tide::Request;
 
+const WOLT_TRACKING_URL: &str =
+    "https://consumer-api.wolt.com/order-tracking-api/v1/details/tracking-code/track/";
+
 pub async fn food_endpoint_server(tx: Sender<Url>) -> tide::Result<()> {
     let mut app = tide::new();
     app.at("/tracking")
@@ -15,9 +18,13 @@ pub async fn food_endpoint_server(tx: Sender<Url>) -> tide::Result<()> {
 
 async fn start_tracking(tx: Sender<Url>, mut req: Request<()>) -> tide::Result {
     let tracking: Tracking = req.body_form().await?;
+    //get last part of url
+
+    let tracking_id = tracking.url.path_segments().unwrap().last().unwrap();
+    let api_url = Url::parse(&format!("{}{}", WOLT_TRACKING_URL, tracking_id)).unwrap();
     // Pass tracking url to the worker thread
-    tx.send(tracking.url.clone()).unwrap();
-    Ok(format!("Got it! Tracking food delivery from {}", tracking.url).into())
+    tx.send(api_url).unwrap();
+    Ok(format!("Got it! Tracking food delivery").into())
 }
 
 #[derive(Debug, Deserialize)]
