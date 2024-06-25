cae5d9908592798e9a8f259a92c2e95111cc6495
Sverre Johann Bjørke <sverrejb@gmail.com>
2024-06-25 22:42:27 +0200

    made it somehow work on android

--- src/lib.rs
+++ src/lib.rs
@@ -20,18 +20,21 @@ pub mod ui {
 #[folder = "img/"]
 struct StaticAssets;
 
-fn main() -> Result<(), PlatformError> {
+
+#[no_mangle]
+fn android_main(app: slint::android::AndroidApp) {
+    slint::android::init(app).unwrap();
     env_logger::init();
     info!("Starting up...");
 
     let main_window = MainWindow::new().unwrap();
 
-    forecast::setup(&main_window);
+    //forecast::setup(&main_window);
     xkcd::setup(&main_window);
-    food::setup(&main_window);
-    calendar::setup(&main_window);
+    //food::setup(&main_window);
+    //calendar::setup(&main_window);
     //we need to store the timer in a variable to prevent it from being dropped
     let _t = datetime::setup(&main_window);
 
-    main_window.run()
+    main_window.run();
 }
