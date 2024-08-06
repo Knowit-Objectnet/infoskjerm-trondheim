6ae615fd31f283dda8dd4793385bfef7ec89475a
andpas <anders.pasche@knowit.no>
2024-08-06 22:50:44 +0200

    Working bus tables filtered by quays.

--- src/transportation/mod.rs
+++ src/transportation/mod.rs
@@ -8,8 +8,6 @@ use std::thread;
 
 use chrono::{Local, TimeDelta, Utc};
 use graphql_client::{GraphQLQuery, Response};
-use log::info;
-use log::kv::Source;
 use reqwest;
 use reqwest::header;
 use slint::{ComponentHandle, Image, Rgba8Pixel, SharedPixelBuffer, SharedString, VecModel, Weak};
@@ -22,11 +20,7 @@ use crate::ui::{MainWindow, StopPlaceData, StopPlaceDataRow};
 const BASE_URL: &str = "https://api.entur.io/journey-planner/v3/graphql";
 // https://developer.entur.org/pages-intro-authentication
 const ET_CLIENT: &str = "knowit-objectnet-trd-infoscreen";
-const PRINSENS_GATE: &str = "NSR:StopPlace:41613";
-const PRINSENS_GATE_P1: &str = "NSR:Quay:71184";
-const PRINSENS_GATE_P2: &str = "NSR:Quay:71181";
-const DEFAULT_TIME_RANGE: i64 = 72100;
-const DEFAULT_NUMBER_OF_DEPARTURES: i64 = 10;
+const DEFAULT_NUMBER_OF_DEPARTURES: i64 = 8;
 
 type Date = String;
 type DateTime = chrono::DateTime<Utc>;
@@ -52,62 +46,41 @@ async fn transportation_worker_loop(window: Weak<MainWindow>) {
     loop {
         match read_tracked_stops() {
             Ok(tracked_stops) => {
-                let all_stops_data: VecModel<StopPlaceData> = VecModel::default();
-                
+                let mut all_filtered_quays: Vec<QueryStopPlaceQuays> = Vec::new();
+
                 for tracked_stop in tracked_stops.stops {
-                    // 1. Run GraphQL Query
+                    // Run GraphQL Query
                     let result = get_stop_place(tracked_stop.clone()).await;
                     let stop_place = match result {
                         Ok(response) => match response.stop_place {
                             Some(stop_place) => stop_place,
                             None => {
-                                info!("No stop places returned from server. Check if the StopPlace-ID exists.");
+                                println!("No stop places returned from server. Check if the StopPlace-ID exists.");
                                 continue
                             }
                         }
                         Err(_) => {
-                            info!("Error from server. Check query.");
+                            println!("Error from server. Check query.");
                             continue
                         }
                     };
-                    
-                    // 2. Filter on tracked quays and lines
+
+                    // Filter on tracked quays and lines
                     let filtered_quays = match filter_by_quays_and_lines(stop_place, tracked_stop) {
                         None => {
-                            info!("No quays left after filtering. Check if Quay-IDs exist on this StopPlace.");
+                            println!("No quays left after filtering. Check if Quay-IDs exist on this StopPlace.");
                             continue
                         },
                         Some(filtered_quays) => filtered_quays
                     };
-                    
-                    // 3. Construct StopPlaceData and place in Vec
-                    for quay in filtered_quays {
-                        let stop_place_data_rows: VecModel<StopPlaceDataRow> = VecModel::default();
-                        
-                        for estimated_call in quay.estimated_calls {
-                            match extract_relevant_values(estimated_call) {
-                                None => {
-                                    info!("Unable to extract values from estimated call. Skipping.");
-                                    continue
-                                }
-                                Some(row) => stop_place_data_rows.push(row) 
-                            };
-                        }
-                        
-                        let stop_place_data = StopPlaceData {
-                            stopName: SharedString::from(format!("{} {}", quay.name, quay.public_code.unwrap_or(String::from("")))),
-                            stopDataRows: Rc::new(stop_place_data_rows).into()
-                        };
 
-                        all_stops_data.push(stop_place_data);
-                    }
+                    all_filtered_quays.extend(filtered_quays);
                 }
-                
-                // Display Vec of StopPlaceData
-                display_transportation(&window, all_stops_data);
+
+                display_transportation(&window, all_filtered_quays);
             }
             Err(_) => {
-                info! { "Unable to read file containing tracked stops information." }
+                println! { "Unable to read file containing tracked stops information." }
             }
         };
         
@@ -115,14 +88,38 @@ async fn transportation_worker_loop(window: Weak<MainWindow>) {
     }
 }
 
-fn display_transportation(window_weak: &Weak<MainWindow>, all_stops_data: VecModel<StopPlaceData>) {
-    /*
-    window_weak
-        .upgrade_in_event_loop(move |window: MainWindow| {
-            window.set_stopPlacesData(Rc::new(all_stops_data).into())
-        })
-        .unwrap();
-    */
+fn display_transportation(window: &Weak<MainWindow>, filtered_quays: Vec<QueryStopPlaceQuays>) {
+    let _ = window.upgrade_in_event_loop(|window: MainWindow| {
+
+        let all_stops_data: VecModel<StopPlaceData> = VecModel::default();
+
+        for quay in filtered_quays {
+            let quay_name = quay.name;
+            let quay_public_code = quay.public_code.unwrap_or(String::from(""));
+            
+            println!("Processing quay {} {}", quay_name, quay_public_code);
+            let stop_place_data_rows: VecModel<StopPlaceDataRow> = VecModel::default();
+
+            for estimated_call in quay.estimated_calls {
+                match extract_relevant_values(estimated_call) {
+                    None => {
+                        println!("Unable to extract values from estimated call. Skipping.");
+                        continue
+                    }
+                    Some(row) => stop_place_data_rows.push(row)
+                };
+            }
+
+            let stop_place_data = StopPlaceData {
+                stopName: SharedString::from(format!("{} {}", quay_name, quay_public_code)),
+                stopDataRows: Rc::new(stop_place_data_rows).into()
+            };
+
+            all_stops_data.push(stop_place_data);
+        }
+
+        window.set_stopPlacesData(Rc::new(all_stops_data).into());
+    });
 }
 
 fn read_tracked_stops() -> Result<TrackedStops, Box<dyn Error>> {
@@ -138,21 +135,21 @@ fn read_tracked_stops() -> Result<TrackedStops, Box<dyn Error>> {
 }
 
 fn filter_by_quays_and_lines(query_stop_place: QueryStopPlace, tracked_stop: TrackedStop) -> Option<Vec<QueryStopPlaceQuays>> {
-    let all_query_quays: Vec<&QueryStopPlaceQuays> = match query_stop_place.quays {
+    let all_query_quays: Vec<QueryStopPlaceQuays> = match query_stop_place.quays {
         None => {
-            info!("No quays for stop with ID {}", query_stop_place.id);
+            println!("No quays for stop with ID {}", query_stop_place.id);
             return None
         },
-        Some(quays) => quays.iter().flatten().collect::<Vec<&QueryStopPlaceQuays>>()
+        Some(quays) => quays.into_iter().flatten().collect::<Vec<QueryStopPlaceQuays>>()
     };
     
-    let tracked_quays: Vec<&TrackedQuay> = tracked_stop.quays.iter().flatten().collect();
+    let tracked_quays: Vec<TrackedQuay> = tracked_stop.quays.into_iter().flatten().collect();
     let mut picked_query_quays: Vec<QueryStopPlaceQuays> = Vec::new();
     
     for tracked_quay in tracked_quays {
-        for query_quay in all_query_quays {
+        for query_quay in all_query_quays.clone() {
             if tracked_quay.id == query_quay.id {
-                picked_query_quays.push(query_quay.clone())
+                picked_query_quays.push(query_quay)
             }
         }
     }
@@ -166,13 +163,13 @@ fn filter_by_quays_and_lines(query_stop_place: QueryStopPlace, tracked_stop: Tra
 fn extract_relevant_values(estimated_call: QueryStopPlaceQuaysEstimatedCalls) -> Option<StopPlaceDataRow> {
     let transport_mode = match &estimated_call.service_journey.journey_pattern {
         None => {
-            info!("No journey pattern found for service journey '{}'", estimated_call.service_journey.public_code.unwrap_or(String::from("unknown")));
+            println!("No journey pattern found for service journey '{}'", estimated_call.service_journey.public_code.unwrap_or(String::from("unknown")));
             return None;
         },
         Some(journey_pattern) => {
             match &journey_pattern.line.transport_mode {
                 None => {
-                    info!("No transport mode found for line '{}'", journey_pattern.line.id);
+                    println!("No transport mode found for line '{}'", journey_pattern.line.id);
                     return None
                 },
                 Some(transport_mode) => {
@@ -197,7 +194,7 @@ fn extract_relevant_values(estimated_call: QueryStopPlaceQuaysEstimatedCalls) ->
 
     let custom_format = match format_departure_time(aimed_departure_time) {
         None => {
-            info!("Unable to format departure time: {}", aimed_departure_time.to_string());
+            println!("Unable to format departure time: {}", aimed_departure_time.to_string());
             return None
         },
         Some(value) => value,
@@ -221,7 +218,6 @@ async fn get_stop_place(tracked_stop: TrackedStop) -> Result<ResponseData, Box<d
 
     let variables = Variables {
         id: tracked_stop.id,
-        time_range: Some(DEFAULT_TIME_RANGE),
         number_of_departures: Some(DEFAULT_NUMBER_OF_DEPARTURES),
     };
     
@@ -249,6 +245,7 @@ fn format_departure_time(departure_time: DateTime) -> Option<String> {
 
     return
     if time_delta < TimeDelta::zero() {
+        println!("Departure time in the past.");
         None
     } else if time_delta < TimeDelta::minutes(1) {
         Some(String::from("Nå"))
